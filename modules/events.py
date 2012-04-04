4d1f2c74444fb1f00072b1ab9f526adb770aa2e4
Michael Babich <michaelbabich7@gmail.com>
2012-04-03 20:35:33 -0400

    Relicensing under the WTFPL, which is a more permissive license.

--- modules/events.py
+++ modules/events.py
@@ -1,18 +1,11 @@
 #    AethBot Events Module
-#    Copyright (C) 2011 Michael Babich
+#    Copyright (C) 2011, 2012 Michael Babich
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    This program is free software. It comes without any warranty, to
+#    the extent permitted by applicable law. You can redistribute it
+#    and/or modify it under the terms of the Do What The Fuck You Want
+#    To Public License, Version 2, as published by Sam Hocevar. See
+#    http://sam.zoy.org/wtfpl/COPYING for more details.
 
 
 # python-irclib modules.
