758a6bcf742f454a15dfeb45b40d89b5e74b9d07
Michael Babich <michaelbabich7@gmail.com>
2012-04-23 21:04:48 -0400

    Making __main__ much cleaner.

--- aethbot/__main__.py
+++ aethbot/__main__.py
@@ -1,37 +1,37 @@
 # Copyright (c) 2011, 2012 Michael Babich
 # See LICENSE.txt or http://www.opensource.org/licenses/mit-license.php
 
+'''This directly launches AethBot and reads in configuration values
+from default.yml or config.yml if the latter exists.
+
+To run this file, use the command ``python -m aethbot``
+'''
 from aethbot import AethBot
 from yaml import load
 
-# Loads the configuration file
 def load_config():
-    # Loads the defaults.
+    '''This turns the YAML file(s) into a configuration dictionary
+    that AethBot recognizes. First, it loads the defaults. Then it
+    tries to read config.yml and update the Connection entry,
+    overriding any defaults with custom values.
+    '''
     default_file = open('default.yml', 'r')
-    default      = load(default_file)
+    config       = load(default_file)
     default_file.close()         
 
-    # Loads the custom options, if they exist.
     try:
         config_file = open('config.yml', 'r')
-        config      = load(config_file)
+        config['Connection'].update(load(config_file)['Connection'])
         config_file.close()
 
     except IOError:
-        config = default
-
-    # Anything missing from the config file is overriden by the defaults.
-    for main_section in default:
-        if main_section not in config:
-            config[main_section] = default[main_section]
-        else:
-            for subsection in default[main_section]:
-                if subsection not in config[main_section]:
-                    config[main_section][subsection] = default[main_section][subsection]
+        pass
 
     return config
 
 def main():
+    '''This directly launches AethBot with a given configuration.
+    '''
     bot = AethBot(load_config())
     bot.start()
 
